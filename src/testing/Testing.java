34ae7c17a813c60aa85daef49c6cc5923bd9f247
YipZhenYang <yipzhenyang03@gmail.com>
2025-05-23 12:21:07 +0800

    setup

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -14,6 +14,8 @@ public class Testing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("hello world");
         // TODO code application logic here
     }
     
