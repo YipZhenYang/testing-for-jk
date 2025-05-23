6f9073dd948585d03f7ebee739d3a70462b3645f
KohJiaHeng <kohjiaheng23@gmail.com>
2025-05-23 12:41:39 +0800

    diao ni 2

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -15,7 +15,7 @@ public class Testing {
      */
     public static void main(String[] args) {
         
-        System.out.println("hello wodiao ni rld");
+        System.out.println("hello wodiao ni 2");
         // TODO code application logic here
     }
     
